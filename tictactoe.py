b8ef4323c9b3ab7f16a524c6714c160769b2eec1
Robert S. Young <robertyounghome@gmail.com>
2019-12-23 10:02:57 -0500

    Added basic stategy for the computer rather than random moves.  The computer will attempt to win when it has one move remaining for three in a row.  The computer will also block the player when the player has two in a row.

--- tictactoe.py
+++ tictactoe.py
@@ -54,6 +54,48 @@ class TicTacToe:
 		# Probably return stats as well
 		return a[r]
 
+	def computer_win_or_block(self, value):
+		i=j=-1
+		for x in range(3):
+			if self.board[x].count(value) > 1:
+				if '' in self.board[x]:
+					j = self.board[x].index('')
+					return x, j
+			if self.board[0][x] == value and self.board[1][x] == value and self.board[2][x] == '':
+				return 2, x
+			if self.board[0][x] == value and self.board[2][x] == value and self.board[1][x] == '':
+				return 1, x
+			if self.board[1][x] == value and self.board[2][x] == value and self.board[0][x] == '':
+				return 0, x
+		# main diagonal
+		if self.board[0][0] == value and self.board[1][1] == value and self.board[2][2] == '':
+			return 2, 2
+		if self.board[0][0] == value and self.board[2][2] == value and self.board[1][1] == '':
+			return 1, 1
+		if self.board[1][1] == value and self.board[2][2] == value and self.board[0][0] == '':
+			return 0, 0
+		# other diagonal
+		if self.board[2][0] == value and self.board[1][1] == value and self.board[0][2] == '':
+			return 0, 2
+		if self.board[2][0] == value and self.board[0][2] == value and self.board[1][1] == '':
+			return 1, 1
+		if self.board[1][1] == value and self.board[0][2] == value and self.board[2][0] == '':
+			return 2, 0
+		return i, j
+
+
+	def get_computer_move(self):
+		i, j = self.computer_win_or_block('O')
+		print(i,j)
+		if (i, j) == (-1,-1):
+			i, j = self.computer_win_or_block('X')
+		if (i,j) == (-1,-1):
+			k = random.randint(0,len(self.moves) - 1)
+			i, j = self.moves[k]
+		self.board[i][j] = 'O'
+		self.moves.remove((i,j))
+		return i, j
+
 	# Interacts with the application controller
 	# Makes a move for the player
 	def player_move(self, i, j):
@@ -67,13 +109,8 @@ class TicTacToe:
 	# Interacts with the application controller
 	# Makes a move for the computer
 	def computer_move(self):
-		k = random.randint(0,len(self.moves) - 1)
-		print(k)
-		i, j = self.moves[k]
-		print(i,j)
-		self.board[i][j] = 'O'
-		del self.moves[k]
-		print(self.moves)
+		print('really inside')
+		i, j = self.get_computer_move()
 		r = self.check_board()
 		caller = f"button{i}{j}"
 		return {'caller': caller, 'value': 'O', 'result': r}
