15887b4943238bfbb61cb2116f62babe8cb435f0
Robert S. Young <robertyounghome@gmail.com>
2020-01-03 09:42:36 -0500

    Added minesweeper in basic form.  Game is operational, although improvements will come.

--- application.py
+++ application.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask import render_template, request, redirect, jsonify
 from tictactoe import TicTacToe
+from minesweeper import Minesweeper
 
 app = Flask(__name__, template_folder='')
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['debug'] = True
 tictactoe = TicTacToe()
+minesweeper = Minesweeper()
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -15,12 +18,14 @@ def index():
 def pick_game():
     yourname = request.form['yourname']
     game = request.form['game']
-    if game == 'tictactoe':
+    if game == 'minesweeper':
+        minesweeper.setName(yourname)
+        minesweeper.reset()
+        return render_template('minesweeper.html', name = yourname, board_size = minesweeper.board_size, board = minesweeper.board)
+    else:
         tictactoe.setName(yourname)
         tictactoe.reset()
         return render_template('tictactoe.html', name = yourname)
-    else:
-    	return render_template('minesweeper.html', name = yourname)
 
 @app.route('/move', methods=['GET', 'POST'])
 def move():
@@ -29,18 +34,34 @@ def move():
     h['caller'] = caller
     return jsonify(h)
 
+@app.route('/mine_move', methods=['GET', 'POST'])
+def mine_move():
+    caller = request.form.get('caller')
+    h = minesweeper.player_move(int(caller[caller.index('-')+1:caller.rindex('-')]), int(caller[caller.rindex('-')+1:]))
+    # cannot jsonify with a tuple key, change to string i + "-" + j
+    w = {}
+    for k in h['updates'].keys():
+        value = h['updates'][k]
+        [i,j] = k
+        w[str(i)+'-'+str(j)] = value
+    h['updates'] = w
+    h['caller'] = caller
+    return jsonify(h)
+
 @app.route('/computer_move', methods=['GET', 'POST'])
 def computer_move():
-    print('inside computer move')
-    # caller = request.form.get('caller')
     h = tictactoe.computer_move()
-    print(h)
     return jsonify(h)
 
 @app.route('/reset', methods=['GET', 'POST'])
 def reset():
     tictactoe.reset()
     return jsonify({})
+
+@app.route('/mine_reset', methods=['GET', 'POST'])
+def mine_reset():
+    minesweeper.reset()
+    return jsonify({'board_size': minesweeper.board_size})
 
 @app.after_request
 def add_header(r):
---
+++ minesweeper.py
@@ -0,0 +1,124 @@
+# minesweeper program
+# three levels Basic, Intermediate, Advanced
+import random, itertools
+BLANK = '0'
+BOMB = '*'
+
+class Minesweeper:
+	def __init__(self, name="Player"):
+		self.name = name
+		self.complexity = [(8,10),(16,24),(24,99)]
+		self.level = 1
+		self.reset()
+
+	def reset(self):
+		self.board_size = self.complexity[self.level][0]
+		print(self.board_size)
+		self.board = [[BLANK for _ in range(self.board_size)] for _ in range(self.board_size)]
+		self.show_board = [[BLANK for _ in range(self.board_size)] for _ in range(self.board_size)]
+		self.moves = []
+		self.bombs = []
+		self.start = True
+		for i in range(self.board_size):
+			for j in range(self.board_size):
+				self.moves.append((i,j))
+
+	def makeMove(self, i, j):
+		updates = {}
+		updates[(i,j)] = self.board[i][j]
+		print(updates)
+		if self.board[i][j] == BOMB:
+			return updates
+		self.moves.remove((i,j))
+		if self.board[i][j] == BLANK:
+			updates = self.finish_updates(updates)
+		return updates
+
+	def makeFirstMove(self, i, j):
+		updates={}
+		for i1, j1 in itertools.product([0,1,-1],[0,1,-1]):
+			i2 = i1 + i
+			j2 = j1 + j
+			if i2 >= 0 and j2 >= 0 and i2 < self.board_size and j2 < self.board_size:
+				self.moves.remove((i2,j2))
+				updates[(i2,j2)] = BLANK
+		self.place_bombs()
+		self.place_numbers()
+		updates = self.finish_updates(updates)
+		return updates
+
+	def finish_updates(self, updates):
+		bfs = []
+		for i, j in updates.keys():
+			updates[(i,j)] = self.board[i][j]
+			if self.board[i][j] == BLANK:
+				bfs.append((i,j))
+		while len(bfs):
+			i,j = bfs.pop()
+			for i1, j1 in itertools.product([0,1,-1],[0,1,-1]):
+				i2 = i1 + i
+				j2 = j1 + j
+				if i2 >= 0 and j2 >= 0 and i2 < self.board_size and j2 < self.board_size:
+					if (i2,j2) in self.moves:
+						self.moves.remove((i2,j2))
+						self.show_board[i2][j2] = self.board[i2][j2]
+						updates[(i2,j2)] = self.board[i2][j2]
+						if self.board[i2][j2] == BLANK:
+							bfs.append((i2,j2))
+		return updates
+
+	def setName(self, name):
+		self.name = name
+
+	def print_board(self):
+		for i in range(self.board_size):
+			for j in range(self.board_size):
+				print(self.board[i][j], end = "")
+			print()
+
+	def place_bombs(self):
+		number_of_bombs = self.complexity[self.level][1]
+		while number_of_bombs:
+			x = random.randint(0,len(self.moves) - 1)
+			i, j = self.moves[x]
+			del self.moves[x]
+			self.bombs.append((i,j))
+			self.board[i][j] = BOMB
+			number_of_bombs-=1
+
+	def place_numbers(self):
+		for i in range(self.board_size):
+			for j in range(self.board_size):
+				if self.board[i][j] != BOMB:
+					for i1, j1 in itertools.product([0,1,-1],[0,1,-1]):
+						i2 = i1 + i
+						j2 = j1 + j
+						if i2 >= 0 and j2 >= 0 and i2 < self.board_size and j2 < self.board_size:
+							self.board[i][j] = str(int(self.board[i][j]) + (self.board[i2][j2] == BOMB))
+
+	# Interacts with the application controller
+	# Makes a move for the player
+	def player_move(self, i, j):
+		if self.start:
+			updates = self.makeFirstMove(i, j)
+			self.start = False
+		else:
+			updates = self.makeMove(i, j)
+		r = ""
+		if (i,j) in self.bombs:
+			r = f"{self.name} loses!"
+		if len(self.moves) == 0:
+			r = f"{self.name} wins!"
+		return {'updates': updates, 'result': r}
+
+	# def bfs_find_blanks(self, i, j):
+	# 	bfs = [(i,j)]
+	# 	while len(bfs):
+	# 		i,j = bfs.pop(0)
+	# 		for i1, i2 in [(1,0)]
+
+if __name__ == '__main__':
+	game = Minesweeper()
+	game.print_board()
+	game.makeFirstMove(5,3)
+	game.print_board()
